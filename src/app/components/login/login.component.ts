48c132477bae993803a61c40b9913f07379a294b
Marcos <marcos.monzon@alten.es>
2020-03-24 16:55:42 +0100

    feat: login service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/user.model';
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginUserData: User = {
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(private _auth: AuthService) { }
 
   ngOnInit() {
   }
 
+  loginUser() {
+    console.log('form: ', this.loginUserData);
+    this._auth.loginUser(this.loginUserData)
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      )
+  }
 }
